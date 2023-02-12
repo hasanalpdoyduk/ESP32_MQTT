bd1842d3a2ea6b125eb8dcd8a24005bd0b2ab610
Hasan Alp Doyduk <67971540+hasanalpdoyduk@users.noreply.github.com>
2023-02-12 05:40:49 +0300

    Create temp_humid_trial3.ino

---
+++ temp_humid_trials/temp_humid_trial3.ino
@@ -0,0 +1,127 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <Wire.h>
+
+// Replace the next variables with your SSID/Password combination
+const char* ssid = "REPLACE_WITH_YOUR_SSID";
+const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+
+// Add your MQTT Broker IP address/Username/Password combination
+const char* mqtt_server = "YOUR_MQTT_BROKER_IP_ADDRESS";
+const char* brokerUser = "REPLACE_WITH_YOUR_BROKER_USERNAME";
+const char* brokerPass = "REPLACE_WITH_YOUR_BROKER_PASSWORD";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+// LED Pin
+const int ledPin = 4;
+
+void setup() {
+  Serial.begin(115200);
+  // default settings
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
+  pinMode(ledPin, OUTPUT);
+}
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.print("\nConnecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print("] ");
+  String messageLed;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageLed += (char)message[i];
+  }
+  Serial.println();
+
+  // Feel free to add more if statements to control more GPIOs with MQTT
+
+  // If a message is received on the topic esp32/output, you check if the message is either "on" or "off". 
+  // Changes the output state according to the message
+  if (String(topic) == "esp32/output") {
+    Serial.print("Changing output to ");
+    if(messageLed == "on"){
+      Serial.println("on");
+      digitalWrite(ledPin, HIGH);
+    }
+    else if(messageLed == "off"){
+      Serial.println("off");
+      digitalWrite(ledPin, LOW);
+    }
+  }
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP32Client", String(brokerUser), String(brokerPass))) {
+      Serial.println("connected");
+      // Subscribe
+      client.subscribe("esp32/output");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  if (now - lastMsg > 5000) {
+    lastMsg = now;
+  }
+}
+
+/*
+  <###><###>EXTRA<###><###>
+<##><##><##>BONUS<##><##><##>
+
+ long now = millis();
+  if (now - lastMsg > 5000) {
+     value++;
+     snprintf(msg, 75, "Count: %ld", value);
+     Serial.print("Sending messages: ");
+     Serial.println(msg);
+     client.publish(Topic, msg);
+     lastMsg = millis();
+*/  
+
+}
